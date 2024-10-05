1b042aee190082f2b3412437ef24cffe6e9b4afe
unknown <viniciusreis3105@gmail.com>
2024-10-05 14:16:39 -0300

    feat: add index file

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { AudioSDK } from "./audio";
+import { ClipboardSDK } from "./clipboard";
+import { GpsSDK } from "./gps";
+import { NotificationSDK } from "./notifications";
+import { StorageSDK } from "./storage";
+import { VibrationSDK } from "./vibration";
+import { VideoSDK } from "./video";
+
+export default {
+	AudioSDK,
+	VideoSDK,
+	GpsSDK,
+	NotificationSDK,
+	StorageSDK,
+	ClipboardSDK,
+	VibrationSDK,
+};
